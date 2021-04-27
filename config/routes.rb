ce2b647e033bb13bb4a6fe27cdc8f3033182632d
Phan Quang Tien <phan.quang.tien@framgia.com>
2021-04-28 00:04:42 +0700

    Define APi

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :news, only: %i[index show]
+    end
+  end
+
   get "*path", to: 'y_combinator#index', constraints: { path: /((?!rails|pack).)*/ }
 end
